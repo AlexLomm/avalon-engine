ba9a6f5a4ee3e8d7642f127df17c569dbbee6995
Alexander <alex.lomia@gmail.com>
2018-11-23 22:22:09 +0400

    feat(roles.config): replaced enum values with strings

--- src/configs/roles.config.ts
+++ src/configs/roles.config.ts
@@ -1,27 +1,27 @@
 export enum Loyalty {
-  Unknown,
+  Unknown = 'Unknown',
   //
-  Good,
-  Evil
+  Good    = 'Good',
+  Evil    = 'Evil'
 }
 
 export enum RoleId {
-  Unknown,
+  Unknown   = 'Unknown',
   //
-  Merlin,
-  Percival,
-  Servant_1,
-  Servant_2,
-  Servant_3,
-  Servant_4,
-  Servant_5,
-  Mordred,
-  Morgana,
-  Assassin,
-  Oberon,
-  Minion_1,
-  Minion_2,
-  Minion_3,
+  Merlin    = 'Merlin',
+  Percival  = 'Percival',
+  Servant_1 = 'Servant_1',
+  Servant_2 = 'Servant_2',
+  Servant_3 = 'Servant_3',
+  Servant_4 = 'Servant_4',
+  Servant_5 = 'Servant_5',
+  Mordred   = 'Mordred',
+  Morgana   = 'Morgana',
+  Assassin  = 'Assassin',
+  Oberon    = 'Oberon',
+  Minion_1  = 'Minion_1',
+  Minion_2  = 'Minion_2',
+  Minion_3  = 'Minion_3',
 }
 
 export type RolesConfig = { [key in RoleId]: RoleConfig };
